360840e0cc90a1c76df0b8aa2f854bbf70cf3143
shalom <espinalcorreasalome@gmail.com>
2026-07-22 19:45:03 -0500

    apartado visual
    maneja todo lo referente a colores letras  etc

---
+++ demo/tema.py
@@ -0,0 +1,109 @@
+#literal el tema y visual de lasic
+
+#fondo general del demo
+COLOR_FONDO ="#050B1E"
+#barra superior 
+COLOR_BARRA_SUPERIOR= "#161616"
+#panel lateral 
+COLOR_PANEL_LATERAL = "#6C659E"
+#panel exterior de la camara 
+COLOR_CONTENEDOR_CAMARA = "#8582B6"
+#FORNDO DE PRUEBA CAMARA
+COLOR_FONDO_CAMARA ="#F7F7FA"
+#panel de traduccion
+COLOR_PANEL_RESULTADO = "#FFDDC7"
+#boton de inicio
+COLOR_BOTON_INICIAR ="#7359D9"
+#color al pasar el cursor por iniciar 
+COLOR_BOTON_INICIAR_HOVER = "#856FE2"
+#boton de finalizar
+COLOR_BOTON_FINALIZAR ="#AAA4FF"
+#color al pasar el cursor por finalizar
+COLOR_BOTON_FINALIZAR_HOVER = "#BBB6FF"
+#texto oscuro principal
+COLOR_TEXTO_OSCURO= "#130C8B"
+#texto claro
+COLOR_TEXTO_CLARO = "#FFFFFF"
+#texto secundario
+COLOR_TEXTO_SEGUNDARIO= "#E9E7FF"
+# Texto cuando no existe traducción
+COLOR_TEXTO_ESPERA = "#6B6595"
+# Mensaje de error o seña no reconocida
+COLOR_ERROR = "#B83D56"
+# Estado correcto
+COLOR_CORRECTO = "#3D8F70"
+
+#dimeciones de la ventana 
+ANCHO_VENTANA = 1100
+ALTO_VENTANA = 720
+
+ANCHO_MINIMO = 900
+ALTO_MINIMO = 600
+
+#esquinas redondiadas
+
+RADIO_PANEL = 28
+RADIO_BOTON = 24
+RADIO_CAMARA = 25
+RADIO_RESULTADO = 25
+
+#fuentes :)
+FUENTE_TITULO = (
+    "Georgia",
+    31,
+    "normal"
+)
+
+FUENTE_SUBTITULO = (
+   "Georgia",
+    21,
+    "normal"
+)
+
+FUENTE_DESCRIPCION =(
+    "Ariar",
+    15,
+    "normal"
+)
+
+FUENTE_BOTON = (
+    "Ariar",
+    20,
+    "bold"
+)
+
+FUENTE_RESULTADO_TITULO = (
+    "Arial",
+    18,
+    "bold"
+)
+
+FUENTE_RESULTADO = (
+    "Georgia",
+    70,
+    "normal"
+)
+
+FUENTE_ESTADO = (
+    "Arial",
+    13,
+    "normal"
+)
+
+#textos de la aoplicacion
+
+TITULO_APLICACION = "Traductor LSC LASIC"
+
+DESCRIPCION_PROYECTO = (
+    "traductor visual de lengua de señas "
+    "colombiana mediante reconocimiento "
+    "de manos en tiempo real."
+)
+
+TEXTO_INICIAR = "iniciar"
+TEXTO_FINALIZAR = "finalizar"
+TEXTO_RESULTADO = "traduccion"
+TEXTO_ESPERANDO = "-"
+TEXTO_CAMARA_APAGADA = "camara apagada"
+TEXTO_LISTO = "sistema listo"
+TEXTO_CAMARA_ACTIVA = "camara activa "
